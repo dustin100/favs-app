f33639f1832688de8baa95b33bb6177658a4e592
Dustin <dmkelly100@gmail.com>
2020-11-09 14:09:51 -0500

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import LandingPage from './components/pages/LandingPage';
 import CategoryPage from './components/pages/CategoryPage';
 import DashboardPage from './components/pages/DashboardPage';
-
 import Navbar from './components/ui/Navbar';
+
+import { Provider } from 'react-redux';
+import store from './store';
+import { Fragment } from 'react';
 
 const App = () => {
 	return (
-		<Router>
-			<Navbar />
-			<div className='wrapper'>
-				<Switch>
-					<Route exact path='/' component={LandingPage} />
-					<Route exact path='/dashboard' component={DashboardPage} />
-					<Route exact path='/category' component={CategoryPage} />
-				</Switch>
-			</div>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<div className='wrapper'>
+						<Switch>
+							<Route exact path='/' component={LandingPage} />
+							<Route exact path='/dashboard' component={DashboardPage} />
+							<Route exact path='/category' component={CategoryPage} />
+						</Switch>
+					</div>
+				</Fragment>
+			</Router>
+		</Provider>
 	);
 };
 
