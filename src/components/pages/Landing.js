1a0db70eddb619912c66c35578252dc0558c091b
Dustin <dmkelly100@gmail.com>
2020-11-12 10:04:12 -0500

    updated Landing css

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 // Styles
 const useStyles = makeStyles((theme) => ({
 	btn: {
-		...theme.typography.button,
+		...theme.palette.typography.button,
 	},
 }));
 
