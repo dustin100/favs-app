bc731f57b2bbad7931277b3593d48cc4b3b5b86f
Dustin <dmkelly100@gmail.com>
2020-11-16 19:39:24 -0500

    added missing return in route

--- api/routes/users.js
+++ api/routes/users.js
@@ -33,7 +33,9 @@ router.post(
 			let user = await User.findOne({ email: email });
 
 			if (user) {
-				res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'User already exists' }] });
 			}
 
 			user = new User({
