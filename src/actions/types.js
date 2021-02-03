9014d2f2d1c0cfc1a7a240aaae89097f1c8acaf1
Dustin <dmkelly100@gmail.com>
2021-02-03 12:37:53 -0500

    fixed typos in types

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,16 +5,16 @@ export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS,';
-export const LOGIN_FAIL = 'LOGIN_FAIL,';
-export const LOGOUT = 'LOGOUT,';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
 
-export const GET_PROFILE = 'GET_PROFILE,';
-export const PROFILE_ERROR = 'PROFILE_ERROR,';
-export const CLEAR_PROFILE = 'CLEAR_PROFILE,';
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 
-export const GET_CATEGORY = 'GET_CATEGORY,';
-export const CATEGORY_ERROR = 'CATEGORY_ERROR,';
+export const GET_CATEGORY = 'GET_CATEGORY';
+export const CATEGORY_ERROR = 'CATEGORY_ERROR';
 export const UPDATE_CATEGORY = 'UPDATE_CATEGORY';
 export const DELETE_CATEGORY = 'DELETE_CATEGORY';
 export const CLEAR_CATEGORY = 'CLEAR_CATEGORY';
@@ -24,11 +24,11 @@ export const UPDATE_PAGE = 'UPDATE_PAGE';
 export const LOADING_CATEGORY = 'LOADING_CATEGORY';
 
 
-export const GET_ITEM = 'GET_ITEM,';
-export const ITEM_ERROR = 'ITEM_ERROR,';
-export const UPDATE_ITEM = 'UPDATE_ITEM,';
+export const GET_ITEM = 'GET_ITEM';
+export const ITEM_ERROR = 'ITEM_ERROR';
+export const UPDATE_ITEM = 'UPDATE_ITEM';
 export const CLEAR_ITEM = 'CLEAR_ITEM';
-export const UPDATE_ITEM_PAGE = 'UPDATE_ITEM_PAGE,';
+export const UPDATE_ITEM_PAGE = 'UPDATE_ITEM_PAGE';
 export const UPDATE_ITEM_PARAMS = 'UPDATE_ITEM_PARAMS';
 export const LOADING_ITEM = 'LOADING_ITEM';
 
